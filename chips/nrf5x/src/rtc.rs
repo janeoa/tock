94971cb66773b3c301530786fdc5435bda2052b3
Kemalele <kemalele17@gmail.com>
2025-04-14 21:12:59 +0500

    feat: RTC base address for nrf53 chip

--- chips/nrf5x/src/rtc.rs
+++ chips/nrf5x/src/rtc.rs
@@ -12,6 +12,11 @@ use kernel::utilities::registers::{register_bitfields, ReadOnly, ReadWrite, Writ
 use kernel::utilities::StaticRef;
 use kernel::ErrorCode;
 
+#[cfg(feature = "nrf53")]
+const RTC1_BASE: StaticRef<RtcRegisters> =
+    unsafe { StaticRef::new(0x50014000 as *const RtcRegisters) };
+
+#[cfg(feature = "nrf52")]
 const RTC1_BASE: StaticRef<RtcRegisters> =
     unsafe { StaticRef::new(0x50011000 as *const RtcRegisters) };
 
